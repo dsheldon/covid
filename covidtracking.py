9b5e3639c013704cca8c1b5819c455efaa1777b7
Dan Sheldon <sheldon@cs.umass.edu>
2020-03-23 23:53:41 -0400

    Update data sources

--- covidtracking.py
+++ covidtracking.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
+import cachetools.func
 
+@cachetools.func.ttl_cache(ttl=3600)
 def load_us():
     df = pd.read_csv('https://covidtracking.com/api/states/daily.csv')
     df.date = pd.to_datetime(df.date, format='%Y%m%d')
     df = df.set_index('date')
     df = df.drop(columns=['dateChecked'])
+    df['confirmed'] = df['positive'] # useful alias to match other data
     df = df.pivot(columns='state')
     df.columns = df.columns.swaplevel()
     df.sort_index(axis=1, inplace=True)
