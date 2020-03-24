b5ddcd9885e5af26ccc26f281bcb4f8b9acbb1a8
Dan Sheldon <sheldon@cs.umass.edu>
2020-03-24 15:56:30 -0400

    Add routines to download and parse UGA state data

--- states.py
+++ states.py
@@ -1,3 +1,26 @@
+import pandas as pd
+import cachetools.func
+
+def get_url(sheet):
+    book = 'https://docs.google.com/spreadsheets/d/1_mk1J3ElMH5EmPILcrx8s1KqHqdQYXCWroJeKTR3pV4'
+    return f'{book}/gviz/tq?tqx=out:csv&sheet={sheet}'
+
+@cachetools.func.ttl_cache(ttl=3600)
+def uga_traits():
+    traits = pd.read_csv(get_url('state_traits'), thousands = ',')
+    traits = traits.set_index('postalCode')
+    traits.index = traits.index.fillna('tot')  # there is a total line which has values for a few columns
+    not_empty = [not traits[c].isnull().all() for c in traits.columns]
+    traits = traits.loc[:, not_empty]
+    traits['popdensity'] = traits.totalpop / traits.Land_Area_mile2
+    return traits
+    
+@cachetools.func.ttl_cache(ttl=3600)
+def uga_announcements():
+    announcements = pd.read_csv(get_url('state-covid-announcements'))
+    return announcements
+
+
 states = {
         'AK': 'Alaska',
         'AL': 'Alabama',
