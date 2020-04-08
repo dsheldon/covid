6ea13688820ec7c8f54f3ed3f4d8dcb965783081
Dan Sheldon <sheldon@cs.umass.edu>
2020-04-07 21:31:17 -0400

    Factor out utility routines for running models and generating forecast plots

--- run_sir.py
+++ run_sir.py
@@ -1,75 +1,8 @@
 import sys
-
-import jax
-import jax.numpy as np
-from jax.random import PRNGKey
-
-import numpyro
-from numpyro.infer import MCMC, NUTS, Predictive
-
-import pandas as pd
-from models import SEIR_stochastic
-
 import util
 
-def run_place(place):
-    
-    world_data = util.load_world_data()
-    state_data = util.load_state_data()
-    data = dict(world_data, **state_data)  # all data
-
-    model = 'SEIR'
-    prob_model = SEIR_stochastic
-
-    print(f"******* {place} *********")
-    
-    start = pd.Timestamp('2020-03-04')
-    confirmed = data[place]['data'].confirmed[start:]
-    start = confirmed.index.min()
-
-    T = len(confirmed)
-    N = data[place]['pop']
-
-    args = {
-        'N': N,
-        'T': T,
-        'rw_scale': 2e-1,
-        'det_conc': 10,
-    }
-
-    kernel = NUTS(prob_model,
-                  init_strategy = numpyro.infer.util.init_to_median())
-
-    mcmc = MCMC(kernel, 
-                num_warmup=1000, 
-                num_samples=2000, 
-                num_chains=1)
-
-    print("starting MCMC")
-    mcmc.run(jax.random.PRNGKey(2), 
-             obs = confirmed.values,
-             **args)
-
-    mcmc.print_summary()
-    mcmc_samples = mcmc.get_samples()
-
-    # Prior samples for comparison
-    prior = Predictive(prob_model, posterior_samples = {}, num_samples = 1000)
-    prior_samples = prior(PRNGKey(2), **args)
-    
-    # Posterior predictive samples for visualization
-    args['drift_scale'] = 0 # set drift to zero for forecasting
-    post_pred = Predictive(prob_model, posterior_samples = mcmc_samples)
-    post_pred_samples = post_pred(PRNGKey(2), T_future=100, **args)
-
-    util.write_summary(place, mcmc)
-
-    util.save_samples(place,
-                     prior_samples,
-                     mcmc_samples, 
-                     post_pred_samples)
-    
-    
 if __name__ == "__main__": 
     place = sys.argv[1]
-    run_place(place)
+    data = util.load_state_data()
+    util.run_place(data, place)
+    util.gen_forecasts(data, place, show=False)
--- util.py
+++ util.py
@@ -4,6 +4,20 @@ import covidtracking
 import states
 import sys
 
+from models import SEIR_stochastic, plot_forecast, plot_R0
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import jax
+import jax.numpy as np
+from jax.random import PRNGKey
+
+import numpyro
+from numpyro.infer import MCMC, NUTS, Predictive
+
+
+
 
 def load_world_data():
     # world data
@@ -78,6 +92,71 @@ def load_data():
     return data, pop, place_names, state_pop
 
 
+def run_place(data, 
+              place, 
+              start = '2020-03-04',
+              use_hosp = True, 
+              save = True,
+              num_warmup = 1000,
+              num_samples = 1000,
+              num_chains = 1,
+              num_prior_samples = 1000,
+              save_path = 'out'):
+
+    prob_model = SEIR_stochastic
+    
+    print(f"******* {place} *********")
+    
+    confirmed = data[place]['data'].confirmed[start:]
+    hosp = data[place]['data'].hospitalizedCumulative[start:]
+    start = confirmed.index.min()
+
+    T = len(confirmed)
+    N = data[place]['pop']
+
+    args = {
+        'N': N,
+        'T': T,
+        'rw_scale': 2e-1,
+        'use_hosp' : use_hosp
+    }
+
+    kernel = NUTS(prob_model,
+                  init_strategy = numpyro.infer.util.init_to_median())
+
+    mcmc = MCMC(kernel, 
+                num_warmup=num_warmup, 
+                num_samples=num_samples, 
+                num_chains=num_chains)
+
+    print("Running MCMC")
+    mcmc.run(jax.random.PRNGKey(2),
+             obs = confirmed.values,
+             hosp = hosp.values,
+             **args)
+
+    mcmc.print_summary()
+    mcmc_samples = mcmc.get_samples()
+    
+    # Prior samples for comparison
+    prior = Predictive(prob_model, posterior_samples = {}, num_samples = num_prior_samples)
+    prior_samples = prior(PRNGKey(2), **args)
+
+    # Posterior predictive samples for visualization
+    args['rw_scale'] = 0 # disable random walk for forecasting
+    post_pred = Predictive(prob_model, posterior_samples = mcmc_samples)
+    post_pred_samples = post_pred(PRNGKey(2), T_future=100, **args)
+
+    if save:
+        save_samples(place,
+                     prior_samples,
+                     mcmc_samples, 
+                     post_pred_samples,
+                     path=save_path)
+        
+        write_summary(place, mcmc, path=save_path)
+
+        
 def save_samples(place, 
                  prior_samples,
                  mcmc_samples, 
@@ -100,6 +179,7 @@ def write_summary(place, mcmc, path='out'):
         sys.stdout = f
         mcmc.print_summary()
     sys.stdout = orig_stdout
+
     
 def load_samples(place, path='out'):
     
@@ -111,3 +191,52 @@ def load_samples(place, path='out'):
     post_pred_samples = x['post_pred_samples'].item()
     
     return prior_samples, mcmc_samples, post_pred_samples
+
+
+def gen_forecasts(data, 
+                  place, 
+                  start = '2020-03-04', 
+                  load_path = 'out',
+                  save_path = 'vis',
+                  save = True,
+                  use_hosp = True,
+                  show = True):
+    
+    confirmed = data[place]['data'].confirmed[start:]
+    hosp = data[place]['data'].hospitalizedCumulative[start:]
+    start_ = confirmed.index.min()
+
+    T = len(confirmed)
+    N = data[place]['pop']
+
+    prior_samples, mcmc_samples, post_pred_samples = load_samples(place, path=load_path)
+    
+    for scale in ['log', 'lin']:
+        for T in [30, 50, 100]:
+
+            t = pd.date_range(start=start_, periods=T, freq='D')
+
+            fig, ax = plot_forecast(post_pred_samples, T, confirmed, 
+                                    t = t, 
+                                    scale = scale, 
+                                    use_hosp = use_hosp, 
+                                    hosp = hosp)
+            
+            name = data[place]['name']
+            plt.suptitle(f'{name} {T} days ')
+
+            if save:
+                filename = f'{save_path}/{place}_predictive_scale_{scale}_T_{T}.png'
+                plt.savefig(filename)
+                
+            if show:
+                plt.show()
+            
+    fig = plot_R0(mcmc_samples, start_)    
+    plt.title(place)
+    if save:
+        filename = f'{save_path}/{place}_R0.png'
+        plt.savefig(filename)
+
+    if show:
+        plt.show()
