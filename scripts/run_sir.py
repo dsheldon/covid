833fccda32b797a0691d8654510966dbc6f83517
Dan Sheldon <sheldon@swarm2.cm.cluster>
2020-04-26 23:36:27 -0400

    change start to 2020-03-15

--- scripts/run_sir.py
+++ scripts/run_sir.py
@@ -4,5 +4,8 @@ import covid.util as util
 if __name__ == "__main__": 
     place = sys.argv[1]
     data = util.load_state_data()
-    util.run_place(data, place)
-    util.gen_forecasts(data, place, show=False)
+
+    start='2020-03-15'
+
+    util.run_place(data, place, start=start)
+    util.gen_forecasts(data, place, start=start, show=False)
