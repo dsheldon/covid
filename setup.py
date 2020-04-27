8fb09c770e20cd4a12a551a59734bad2113e2870
Dan Sheldon <sheldon@cs.umass.edu>
2020-04-27 07:25:33 -0400

    Pull numpyro from dsheldon/numpyro

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import os
-import sys
 from setuptools import find_packages, setup
-
-try:
-    long_description = open('README.md', encoding='utf-8').read()
-except Exception as e:
-    sys.stderr.write('Failed to read README.md:\n  {}\n'.format(e))
-    sys.stderr.flush()
-    long_description = ''
 
 setup(
     name='covid',
@@ -17,11 +8,12 @@ setup(
     url='https://github.com/dsheldon/covid-19',
     author='Dan Sheldon',
     author_email='sheldon@cs.umass.edu',
-    #install_requires=[
-    # TODO: jaxlib, jax, numpyro, patsy
-    #],
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    install_requires=[
+        'jaxlib>=0.1.45',
+        'patsy>=0.5.1',
+        'jax @ git+https://github.com/google/jax.git#egg=jax',
+        'numpyro @ git+https://github.com/dsheldon/numpyro.git#egg=numpyro'
+    ],
     keywords=' machine learning bayesian statistics',
     license='MIT'
 )
