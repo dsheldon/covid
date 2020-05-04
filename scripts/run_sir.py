2dc5e9f45e1d4a0133ae15a831eb03d08995d00e
Dan Sheldon <sheldon@cs.umass.edu>
2020-05-04 13:10:23 -0400

    add argument parser to accept many more options on command-line for more systematic running and comparison of models

--- scripts/run_sir.py
+++ scripts/run_sir.py
@@ -1,12 +1,52 @@
 import sys
+import argparse
 import covid.util as util
 
-if __name__ == "__main__": 
-    place = sys.argv[1]
-    data = util.load_state_data()
+import covid.models.SEIRD
 
-    start='2020-03-15'
-    end='2020-04-26'
+configs = {}
 
-    util.run_place(data, place, start=start, end=end)
-    util.gen_forecasts(data, place, start=start, show=False)
+configs['test'] =  {
+    'model' : covid.models.SEIRD.SEIRD,
+    'args'  : {'num_samples' : 10,
+               'num_warmup': 10}
+}
+
+configs['SEIRD'] = {
+    'model' : covid.models.SEIRD.SEIRD,
+    'args'  : {}
+}
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Run Bayesian compartmental models.')
+    parser.add_argument('place', help='place to use (e.g., US state)')
+    parser.add_argument('--start', help='start date', default='2020-03-04')
+    parser.add_argument('--end', help='end date', default=None)
+    parser.add_argument('--prefix', help='path prefix for saving results', default='results')
+    parser.add_argument('--config', help='model configuration name', default='SEIRD')
+    
+    args = parser.parse_args()
+
+    if args.config not in configs:
+        print(f'Invalid config: {args.config}. Options are {list(configs.keys())}')
+        exit()
+
+    config = configs[args.config]
+        
+    data = util.load_data()
+    
+    # util.run_place(data,
+    #                args.place,
+    #                start=args.start,
+    #                end=args.end,
+    #                prefix=args.prefix,
+    #                model_type=config['model'],
+    #                **config['args'])
+    
+    util.gen_forecasts(data,
+                       args.place,
+                       start=args.start,
+                       prefix=args.prefix,
+                       show=False)
