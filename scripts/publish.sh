c92292be36e794acd150c42d5befdabeb91c331b
Dan Sheldon <sheldon@swarm2.cm.cluster>
2020-05-05 08:07:19 -0400

    follow symlinks so results directory can be on different filesystem

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,6 @@
 DIR=${1:-results}
 echo $DIR
 
-find $DIR -name "vis" -exec cp ../vis/index.html {} \;
+find -L $DIR -name "vis" -exec cp ../vis/index.html {} \;
 
 rsync -avz --exclude="*samples*" $DIR/ doppler:/var/www/html/covid/$DIR/
