c2f38e4107e7644c56150bce9dd8d16a6c5eca07
Dan Sheldon <sheldon@cs.umass.edu>
2020-05-08 22:38:49 -0400

    generalize script

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-DIR=${1:-out}
+MODEL=${1:-SEIRD/2020-05-03}
+SRC=${2:-swarm2:covid/scripts/results}
+DST=${3:-results}
 
-rsync -avz swarm2:covid/scripts/$DIR/ $DIR/
+rsync -avz --relative $SRC/./$MODEL/ $DST/
