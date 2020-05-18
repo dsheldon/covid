95f18a3a69bb55bc42e61f3da90715eb6f738714
Dan Sheldon <sheldon@swarm022.cm.cluster>
2020-05-17 22:59:59 -0400

    update configs

--- scripts/configs.py
+++ scripts/configs.py
@@ -1,5 +1,7 @@
 import covid.models.SEIRD
 import covid.models.SEIRD_variable_detection
+import covid.models.SEIRD_incident
+
 
 
 SEIRD = {
@@ -7,7 +9,42 @@ SEIRD = {
     'args'  : {}                # use defaults
 }
 
-SEIRD_variable_detection = {
+
+# 2020-05-03 forecast
+strongest_prior = {
     'model' : covid.models.SEIRD_variable_detection.SEIRD,
-    'args'  : {}                # use defaults
+    'args'  : {
+        'gamma_shape':  100,
+        'sigma_shape':  100
+    }
+}
+
+
+# 2020-05-10 forecast
+fit_dispersion = {
+    'model' : covid.models.SEIRD_incident.SEIRD,
+    'args'  : {
+        'gamma_shape':  100,
+        'sigma_shape':  100
+    }
+}
+
+less_rw_frozen = {
+    'model': covid.models.SEIRD_incident.SEIRD,
+    'args'  : {
+        'gamma_shape':  100,
+        'sigma_shape':  100,
+        'rw_scale': 1e-1,
+        'num_frozen': 5
+    }
+}
+
+less_rw_last_5 = {
+    'model': covid.models.SEIRD_incident.SEIRD,
+    'args'  : {
+        'gamma_shape':  100,
+        'sigma_shape':  100,
+        'rw_scale': 1e-1,
+        'rw_use_last': 5
+    }
 }
