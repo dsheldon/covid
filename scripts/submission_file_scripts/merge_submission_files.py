3ddaefbbc8e4a6e8e75e501f481c97e2581c4928
Dan Sheldon <sheldon@swarm2.cm.cluster>
2020-08-17 08:55:29 -0400

    minor fix

--- scripts/submission_file_scripts/merge_submission_files.py
+++ scripts/submission_file_scripts/merge_submission_files.py
@@ -3,6 +3,6 @@ import sys
 forecast_date = sys.argv[1]  
 csv1=pd.read_csv("../submission_files/incident/"+forecast_date+"-UMass-MechBayes.csv.clean",dtype=str)
 csv2=pd.read_csv("../submission_files/cumulative/"+forecast_date+"-UMass-MechBayes.csv.clean",dtype=str)
-csv2=pd.read_csv("../submission_files/county/"+forecast_date+"-UMass-MechBayes.csv.clean",dtype=str)
-total = csv1.append(csv2)
+csv3=pd.read_csv("../submission_files/county/"+forecast_date+"-UMass-MechBayes.csv.clean",dtype=str)
+total = csv1.append(csv2).append(csv3)
 total.to_csv("../submission_files/total/"+forecast_date+"-UMass-MechBayes.csv",index=False)
