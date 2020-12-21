A couple tweaks for to fix numerical stability issues encountered today: 1) reduce constraint on initial state variables (I, E, H, D) to 1e-4 * N. 2) Increase max # of iterations for ode solver (unsure if this matters).
Some data adjustments for 2020-12-20 forecasts

New configs for fixing numerical stability issues

covid/compartment.py
@@ -32,7 +32,7 @@ class CompartmentModel(object):
         
     
     @classmethod
-    def _run_static(cls, T, x0, theta, rtol=1e-5, atol=1e-3, mxstep=500):
+    def _run_static(cls, T, x0, theta):
         '''
         x0 is shape (d,)
         theta is shape (nargs,)
@@ -42,7 +42,7 @@ class CompartmentModel(object):
 
     
     @classmethod
-    def _run_time_varying(cls, T, x0, theta, rtol=1e-5, atol=1e-3, mxstep=500):
+    def _run_time_varying(cls, T, x0, theta, rtol=1e-5, atol=1e-3, mxstep=4000):
         
         theta = tuple(np.broadcast_to(a, (T-1,)) for a in theta)
 